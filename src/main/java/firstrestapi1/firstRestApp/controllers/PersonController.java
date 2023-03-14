0a108754984925fec77bb50cd4e8ee8eb0bfb2b0
incompr <88598531+incompr@users.noreply.github.com>
2023-03-14 21:28:29 +0300

    DTO added

--- src/main/java/firstrestapi1/firstRestApp/controllers/PersonController.java
+++ src/main/java/firstrestapi1/firstRestApp/controllers/PersonController.java
@@ -1,11 +1,13 @@
 package firstrestapi1.firstRestApp.controllers;
 
+import firstrestapi1.firstRestApp.dto.PersonDTO;
 import firstrestapi1.firstRestApp.exceptions.PersonNotCreatedException;
 import firstrestapi1.firstRestApp.exceptions.PersonNotFoundException;
 import firstrestapi1.firstRestApp.models.Person;
 import firstrestapi1.firstRestApp.services.PersonService;
 import firstrestapi1.firstRestApp.utils.PersonErrorResponse;
 import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,46 +16,50 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
     private final PersonService peopleService;
-
+    private final ModelMapper modelMapper;
     @Autowired
-    public PersonController(PersonService peopleService) {
+    public PersonController(PersonService peopleService, ModelMapper modelMapper) {
+        this.modelMapper=modelMapper;
         this.peopleService = peopleService;
     }
 
     @GetMapping
-    public List<Person> getPeople() {
+    public List<PersonDTO> getAllPersons() {
 
-        return peopleService.findAll();//Jackson автоматически конвертирует эти объекты в JSON
+        return peopleService.findAll().stream()
+                .map(this::converToPersonDTO)
+                .collect(Collectors.toList());//Jackson автоматически конвертирует эти объекты в JSON
     }
 
     @GetMapping("/{id}")
-    public Person getPerson(@PathVariable("id") int id) {
+    public PersonDTO getPerson(@PathVariable("id") int id) {
 
-        return peopleService.findOne(id);        //джексон автоматически конвертирует в джейсон
+        return converToPersonDTO(peopleService.findOne(id));        //джексон автоматически конвертирует в джейсон
     }
 
 
-
     @PostMapping
-    public ResponseEntity<HttpStatus> create(@RequestBody @Valid Person person, BindingResult bindingResult) {
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid PersonDTO personDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             StringBuilder errorMsg = new StringBuilder();
             List<FieldError> errors = bindingResult.getFieldErrors();
             for (FieldError error : errors) {
                 errorMsg.append(error.getField()).append(" - ").append(error.getDefaultMessage()).append(";");
             }
-        throw new PersonNotCreatedException(errorMsg.toString());
+            throw new PersonNotCreatedException(errorMsg.toString());
 
         }
-        peopleService.save(person);
+        peopleService.save(convertToPerson(personDTO));
         return ResponseEntity.ok(HttpStatus.OK);
     }
+
 
     @ExceptionHandler
     private ResponseEntity<PersonErrorResponse> handleException(PersonNotFoundException ex) {
@@ -63,6 +69,7 @@ public class PersonController {
         );
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);//404
     }
+
     @ExceptionHandler
     private ResponseEntity<PersonErrorResponse> handleException(PersonNotCreatedException ex) {
         PersonErrorResponse response = new PersonErrorResponse(
@@ -71,6 +78,21 @@ public class PersonController {
         );
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);//400
 
+    }
+
+    private Person convertToPerson(PersonDTO personDTO) {
+
+     //   ModelMapper modelMapper = new ModelMapper();
+
+     /*   Person person = new Person();
+        person.setName(personDTO.getName());
+        person.setAge(personDTO.getAge());
+        person.setEmail(personDTO.getEmail());*/
+
+        return modelMapper.map(personDTO, Person.class);
+    }
+    private PersonDTO converToPersonDTO(Person person){
+        return  modelMapper.map(person, PersonDTO.class);
     }
 
 
--- src/main/java/firstrestapi1/firstRestApp/services/PersonService.java
+++ src/main/java/firstrestapi1/firstRestApp/services/PersonService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,7 +42,17 @@ public class PersonService {
 
     @Transactional
     public void save(Person person){
+        enrichPerson(person);
         personRepository.save(person);
+    }
+
+    private void enrichPerson(Person person) {
+
+
+        person.setCreatedAt(LocalDateTime.now());
+        person.setUpdatedAt(LocalDateTime.now());
+        person.setCreatedWho("ADMIN");
+
     }
 
 }
